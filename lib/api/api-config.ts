1bce13dd77572b2cc260bb7ad6f8d560be076f1c
ckddlf <53569322+ckddlf@users.noreply.github.com>
2026-02-04 14:47:57 +0900

    chore: configure API settings for production environment

--- lib/api/api-config.ts
+++ lib/api/api-config.ts
@@ -2,6 +2,9 @@
 
 export const API_URL =
   process.env.NEXT_PUBLIC_BACKEND_API_URL || 'https://classhub.site';
-
+/*
 export const USE_MOCK =
   (process.env.NEXT_PUBLIC_USE_MOCK ?? 'true') === 'true';
+*/
+export const USE_MOCK = false;
+//export const API_URL = 'http://localhost:8080';
