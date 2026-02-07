c20addb9417d0954a00a1395e3339f7976f380a7
ChaeEunLee00 <chaeeun6437@unist.ac.kr>
2026-02-07 17:53:40 +0900

    빌드 에러 해결

--- lib/api/mock/reservation.api.ts
+++ lib/api/mock/reservation.api.ts
@@ -29,7 +29,7 @@ export const reservationApiMock: ReservationApi = {
       reservationStatus: 'RESERVED',
       classCode: classItem.classCode ?? '',
       classTitle: classItem.name ?? '',
-      classCode: classItem.classCode ?? '',
+
       classImageUrl: classItem.imageUrls?.[0] ?? '',
       classLocation: classItem.location ?? '',
       date: session.date ?? '',
