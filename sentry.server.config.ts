4887b2bff6d0b579444eb7646de96f4a6efea3bd
ckddlf <53569322+ckddlf@users.noreply.github.com>
2026-03-15 19:42:15 +0900

    feat: 프론트엔드 Sentry 에러 모니터링 추가

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://2751f933174363d5677e569e1d164652@o4511047558299648.ingest.us.sentry.io/4511048072101888",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
